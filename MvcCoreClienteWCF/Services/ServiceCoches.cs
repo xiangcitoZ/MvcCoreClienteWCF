803e1c383ee5b1448f8c552bf286de3d78eec563
xiangcitoZ <pzxiang1@gmail.com>
2023-03-30 09:30:09 +0200

    30/03

---
+++ MvcCoreClienteWCF/Services/ServiceCoches.cs
@@ -0,0 +1,29 @@
+﻿using ReferenceCoches;
+
+namespace MvcCoreClienteWCF.Services
+{
+    public class ServiceCoches
+    {
+        private CochesContractClient client;
+
+        public ServiceCoches(CochesContractClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<Coche[]> GetCochesAsync()
+        {
+            Coche[] cars = 
+                await this.client.GetCochesAsync();
+            return cars;
+        }
+
+        public async Task<Coche> FindCocheAsync(int idcoche)
+        {
+            Coche car =
+                await this.client.FindCocheAsync(idcoche);
+            return car;
+        }
+
+    }
+}
